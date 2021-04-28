7a7655f7f26f3fa6efd136224bf881e104f860b0
Liliya Ignatova <liliya.oignatova@gmail.com>
2021-04-28 19:22:20 +0300

    fixed adding entries to file properties.csv

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ from urls import url_dictionary
 startTime = datetime.now()
 
 
-def extract_properties(bodiesOnPage, list_so_far):
-    for body in bodiesOnPage:
+def extract_properties(entries_on_page, list_so_far):
+    for body in entries_on_page:
         num_of_rows = len(body.find_all('tr'))
         if num_of_rows > 2:
             price = body.find('div', class_='price').text.strip().split(' ')
@@ -39,34 +39,33 @@ def find_pages(current_page, last_p, soup, real_estates):
         current_page += 1
     return real_estates
 
+
+f = codecs.open('./properties.csv', 'w', encoding='utf-8-sig')
+csv_writer = csv.DictWriter(f, fieldnames=('region', 'num_of_entries', 'avg_p', 'mean_p'), lineterminator='\n')
+csv_writer.writeheader()
+
 for url in url_dictionary.values():
     baseUrl = url
-
-    #baseUrl = 'https://www.imot.bg/pcgi/imot.cgi?act=3&slink=6mbmpd&f1='
     curr_page = 1
     currUrl = baseUrl + str(curr_page)
     body_text = requests.get(currUrl, timeout=5, headers={"Accept-Language": "bg-BG, bg;q=0.5"})
     properties={}
+
     if body_text.status_code == 200:
         soup = BeautifulSoup(body_text.content.decode('windows-1251'), 'lxml')
-        last_page = int(
-            soup.find('span', class_='pageNumbersInfo')
-                .text
-                .split(' ')[-1]
-        )
+        last_page = int(soup.find('span', class_='pageNumbersInfo').text.split(' ')[-1])
         final_list_results = find_pages(curr_page, last_page, soup, properties)
-        with codecs.open('./properties.csv', 'w', encoding='utf-8-sig') as f:
-            csv_writer = csv.DictWriter(f, fieldnames=('region', 'num_of_entries', 'avg_p', 'mean_p'), lineterminator='\n')
-            csv_writer.writeheader()
-            for key, value in final_list_results.items():
-                csv_writer.writerow({
-                    'region': key,
-                    'num_of_entries': len(value),
-                    'avg_p': str(int(sum(value) / len(value))),
-                    'mean_p': str(int(statistics.median(value)))
-                })
+
+        for key, value in final_list_results.items():
+            csv_writer.writerow({
+                'region': key,
+                'num_of_entries': len(value),
+                'avg_p': str(int(sum(value) / len(value))),
+                'mean_p': str(int(statistics.median(value)))
+            })
     else:
         print('404 error')
 
+f.close()
 print(datetime.now() - startTime)
 
