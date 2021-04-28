488069f0b10b4948c9a05abfcf6e860719ed0a1e
Liliya Ignatova <liliya.oignatova@gmail.com>
2021-04-28 20:30:55 +0300

    added error handling

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import requests
 import csv
 import codecs
 from urls import url_dictionary
+from urllib.error import HTTPError, URLError
 
 
 startTime = datetime.now()
@@ -14,17 +15,21 @@ def extract_properties(entries_on_page, list_so_far):
     for body in entries_on_page:
         num_of_rows = len(body.find_all('tr'))
         if num_of_rows > 2:
-            price = body.find('div', class_='price').text.strip().split(' ')
-            if price[0].isdigit() and price[1].isdigit():
-                price = int(price[0] + price[1])
-                test = body.find_all('tr')[3].text.strip()
-                sq_m = int(test.split(' ')[0])
-                price_per_sqm = price / sq_m
-                region = body.find('a', class_='lnk2')
-                region = region.text.split(', ')[1]
-                if f'{region}' not in list_so_far:
-                    list_so_far[f'{region}'] = []
-                list_so_far[f'{region}'].append(int(price_per_sqm))
+            try:
+                price = body.find('div', class_='price').text.strip().split(' ')
+                if price[0].isdigit() and price[1].isdigit():
+                    price = int(price[0] + price[1])
+                    description = body.find_all('tr')[3].text.strip()
+                    sq_m = int(description.split(' ')[0])
+                    price_per_sqm = price / sq_m
+                    region = body.find('a', class_='lnk2')
+                    region = region.text.split(', ')[1]
+                    if f'{region}' not in list_so_far:
+                        list_so_far[f'{region}'] = []
+                    list_so_far[f'{region}'].append(int(price_per_sqm))
+            except (AttributeError, KeyError) as ex:
+                print(ex)
+                continue
     return list_so_far
 
 
@@ -32,8 +37,13 @@ def find_pages(current_page, last_p, soup, real_estates):
     while current_page <= last_p:
         if current_page > 1:
             c_url = baseUrl + str(current_page)
-            body = requests.get(c_url, timeout=5, headers={"Accept-Language": "bg-BG, bg;q=0.5"})
-            soup = BeautifulSoup(body.content.decode('windows-1251'), 'lxml')
+            try:
+                body = requests.get(c_url, timeout=5, headers={"Accept-Language": "bg-BG, bg;q=0.5"})
+                soup = BeautifulSoup(body.content.decode('windows-1251'), 'lxml')
+            except (URLError, HTTPError) as err:
+                print(err)
+                continue
+
         props = soup.find('table')
         real_estates = extract_properties(props.find_all('table', attrs={'width': '660'}), real_estates)
         current_page += 1
@@ -48,23 +58,25 @@ for url in url_dictionary.values():
     baseUrl = url
     curr_page = 1
     currUrl = baseUrl + str(curr_page)
-    body_text = requests.get(currUrl, timeout=5, headers={"Accept-Language": "bg-BG, bg;q=0.5"})
     properties={}
-
-    if body_text.status_code == 200:
+    try:
+        body_text = requests.get(currUrl, timeout=5, headers={"Accept-Language": "bg-BG, bg;q=0.5"})
         soup = BeautifulSoup(body_text.content.decode('windows-1251'), 'lxml')
         last_page = int(soup.find('span', class_='pageNumbersInfo').text.split(' ')[-1])
-        final_list_results = find_pages(curr_page, last_page, soup, properties)
+    except (URLError, HTTPError) as exc:
+        print(exc)
+        continue
 
-        for key, value in final_list_results.items():
-            csv_writer.writerow({
-                'region': key,
-                'num_of_entries': len(value),
-                'avg_p': str(int(sum(value) / len(value))),
-                'mean_p': str(int(statistics.median(value)))
-            })
-    else:
-        print('404 error')
+    final_list_results = find_pages(curr_page, last_page, soup, properties)
+
+    for key, value in final_list_results.items():
+        csv_writer.writerow({
+            'region': key,
+            'num_of_entries': len(value),
+            'avg_p': str(int(sum(value) / len(value))),
+            'mean_p': str(int(statistics.median(value)))
+        })
+
 
 f.close()
 print(datetime.now() - startTime)
