550d838353fff879dcd6b365d79755b8a8abd910
Liliya Ignatova <liliya.oignatova@gmail.com>
2021-04-28 18:51:52 +0300

    import district urls from second file

---
+++ urls.py
@@ -0,0 +1,6 @@
+url_dictionary = {'7-МИ 11-ТИ КИЛОМЕТЪР': 'https://www.imot.bg/pcgi/imot.cgi?act=3&slink=6n5p3y&f1=',
+                  'АБДОВИЦА': 'https://www.imot.bg/pcgi/imot.cgi?act=3&slink=6n5p66&f1=',
+                  'БАНИШОРА': 'https://www.imot.bg/pcgi/imot.cgi?act=3&slink=6n5p88&f1=',
+                  'БЕЛИТЕ БРЕЗИ': 'https://www.imot.bg/pcgi/imot.cgi?act=3&slink=6n5p9f&f1=',
+                  'БЕНКОВСКИ': 'https://www.imot.bg/pcgi/imot.cgi?act=3&slink=6n5pal&f1=',
+                  'БОРОВО': 'https://www.imot.bg/pcgi/imot.cgi?act=3&slink=6n5pbm&f1='}
